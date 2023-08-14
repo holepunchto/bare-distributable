340f818e0345a3ae176492403a78604793f509eb
David Mark Clements <david.mark.clements@gmail.com>
2023-08-14 21:35:38 +0200

    more mappings

--- src/main.js
+++ src/main.js
@@ -18,4 +18,17 @@ const arg = (flag) => {
 }
 const main = arg('--bootstrap-file') || path.join(process.execPath, '../../../boot.js')
 process.versions.node = '20.5.1' // spoof node to workaround deps which throw if not node - TODO: remove platform deps which do this
-Module.load(main, { imports: { fs: 'bare-fs', 'fs/promises': 'bare-fs/promises', 'os': 'bare-os', 'child_process': 'bare-subprocess', events: 'bare-events', http: 'bare-http1' }})
+Module.load(main, {
+  imports: {
+    fs: 'bare-fs',
+    'fs/promises': 'bare-fs/promises',
+    os: 'bare-os',
+    child_process: 'bare-subprocess',
+    events: 'bare-events',
+    http: 'bare-http1',
+    repl: 'bare-repl',
+    url: 'bare-url',
+    'bare-pipe': 'bare-pipe'
+
+  }
+})
