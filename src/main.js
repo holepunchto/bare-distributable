aa53b634cdcaed2ec29340a0d557e96e054bd7ba
David Mark Clements <david.mark.clements@gmail.com>
2023-08-24 14:52:07 +0200

    smoke iteration

--- src/main.js
+++ src/main.js
@@ -23,6 +23,6 @@ const arg = (flag) => {
   }
   return false
 }
-const main = arg('--bootstrap-file') || path.join(process.execPath, '../../../boot.js')
+const main = arg('--bootstrap-file') || path.join(process.execPath, '..', '..', '..', '..', '..', 'boot.js')
 process.versions.node = '20.5.1' // spoof node to workaround deps which throw if not node - TODO: remove platform deps which do this
 Module.load(main)
