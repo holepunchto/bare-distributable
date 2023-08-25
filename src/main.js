55da1ffffc67626fd1201724d15d83a5ab836cba
David Mark Clements <david.mark.clements@gmail.com>
2023-08-25 18:54:32 +0200

    AbortController and AbortSignal prolyfills

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 'use strict'
 const Module = require('module')
 const path = require('path')
+const events = require('events')
 Module._builtins.fs = require('bare-fs')
 Module._builtins['fs/promises'] = require('bare-fs/promises')
 Module._builtins.os = require('bare-os')
@@ -25,4 +26,57 @@ const arg = (flag) => {
 }
 const main = arg('--bootstrap-file') || path.join(process.execPath, '..', '..', '..', '..', '..', 'boot.js')
 process.versions.node = '20.5.1' // spoof node to workaround deps which throw if not node - TODO: remove platform deps which do this
+
+class AbortError extends Error {
+  ABORT_ERR = 20
+  TIMEOUT_ERR = 23
+  code = 20
+  message = 'The operation was aborted'
+}
+class TimeoutError extends AbortError { 
+  code = 23 
+  message = 'The operation was aborted due to a timeout'
+}
+
+class AbortSignal extends events {
+  aborted = false
+  reason = null
+  #onabort = null
+  static timeout (ms) {
+    const ac = new AbortController()
+    setTimeout(() => {
+      ac.abort(new TimeoutError())
+    }, ms)
+    return ac.signal
+  }
+  get onabort () {
+    return this.#onabort
+  }
+  set onabort (fn) {
+    if (this.#onabort) this.off('abort', this.#onabort)
+    this.on('abort', fn)
+  }
+}
+
+class AbortController extends events {
+  constructor () {
+    super()
+    this.signal = new AbortSignal()
+  }
+  abort (reason) {
+    const signal = this.signal
+    if (!signal.aborted) {
+      signal.aborted = true
+      signal.reason = reason || new AbortError()
+      signal.emit('abort')
+    }
+  }
+  throwIfAborted () {
+    if (this.signal.reason) throw this.signal.reason
+  }
+}
+
+global.AbortController = AbortController
+global.AbortSignal = AbortSignal
+
 Module.load(main)
